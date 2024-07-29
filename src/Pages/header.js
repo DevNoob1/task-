488d8d552a62b02a0191c07e03c4c998a8fe2451
Ayush <ayushkr.r1@gmail.com>
2024-07-29 14:26:01 +0530

    animations

--- src/Pages/header.js
+++ src/Pages/header.js
@@ -1,14 +1,11 @@
 import React from 'react'
-import Navbar from '../component/navbar'
 import Section1 from '../component/section1'
 import '../style/header.css'
 
 const header = () => {
     return (
         <div className='header'>
-            {/* <Navbar /> */}
             <Section1 />
-
         </div>
     )
 }
